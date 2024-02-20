f05428df5dbd5ea64be5309d0a4370c8be1d8d6c
itc studio <itcstudio@itcs-iMac.local>
2024-02-20 17:58:11 +0700

    fixed searchpage

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -2,18 +2,24 @@ import { mongooseConnect } from '@/lib/mongoose'
 import { Category } from '@/models/Course'
 import React from 'react'
 import { Categories } from './_components/categories'
+import { SearchInput } from '@/components/search-input'
 
 const SearchPage = async () => {
   await mongooseConnect()
 
   const categories = JSON.parse(JSON.stringify(await Category.find().sort({name: 1})))
   return (
+    <>
+    <div className='px-6 pt-6 md:hidden md:mb-0 block'>
+      <SearchInput />
+    </div>
     <div className='p-6'>
       <Categories 
       items={categories}
       
       />
     </div>
+    </>
   )
 }
 
